7200be1ed2e3b4f8b732522336af8765e46a72a3
TheGeneticsGuy <aaronjtopping@gmail.com>
2024-05-16 23:18:57 -0700

    modified JS to be 1 line of code as per rubric

--- scripts/place.js
+++ scripts/place.js
@@ -6,20 +6,8 @@ document.getElementById("lastmodified").innerHTML = document.lastModified;
 // Method:          calculateWindChill( float , int )
 // What it Does:    Returns the wind chill based on the temperature and wind speed or "N/A" 
 // Formula:         WC (Wind Chill, °F) = 35.74 + 0.6215 × T - 35.75 × V^0.16 + 0.4275 × T × V^0.16
-function calculateWindChill(temperature, windSpeed) {
-
-    // Only calculate if within valid range.
-    if (temperature <= 50 && windSpeed > 3) {
-        let chill =
-            (35.74 + (0.6215 * temperature)) -
-            (35.75 * Math.pow(windSpeed, 0.16)) +
-            (0.4275 * temperature * Math.pow(windSpeed, 0.16));
-
-        return `${chill.toFixed(2)}F`;
-    }
-
-    return "N/A";
-}
+// WRITTEN AS 1 LINE OF CODE AS PER RUBRIC INSTRUCTIONS OF ASSIGNMENT - APOLOGIES FOR BEING UNREADABLE.
+const calculateWindChill = (temperature, windSpeed) => { if (temperature <= 50 && windSpeed > 3) { return `${((35.74 + (0.6215 * temperature)) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16))).toFixed(2)}F`; } return "N/A"; }
 
 // Wind chill
-document.querySelector('#windchill').innerHTML = calculateWindChill(49, 5);
+document.querySelector('#windchill').innerHTML = calculateWindChill(93, 5);
