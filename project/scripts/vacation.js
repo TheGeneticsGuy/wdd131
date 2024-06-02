3e9b612b20c48030361b6d644f03a1a2c678fde6
TheGeneticsGuy <aaronjtopping@gmail.com>
2024-06-01 19:24:48 -0700

    Imgs added, templates being built

--- project/scripts/vacation.js
+++ project/scripts/vacation.js
@@ -1,1 +1,12 @@
 document.querySelector("#currentyear").innerHTML = new Date().getFullYear();
+
+const destinations = [
+    {
+        city: 'New York',
+        description: "Everyone loves New York and there are a million reasons why. Here are but a few: the Statue of Liberty, the Empire State Building, Grand Central Station, the Ellis Island National Monument, the Brooklyn Bridge, the Metropolitan Museum of Art, the Guggenheim Museum, Times Square, Broadway, Greenwich Village, 5th Avenue and Central Park.",
+        avgTemp: [61, 75],
+        avgPrec: 4,
+        dayOfTrip: 1
+
+    }
+]
